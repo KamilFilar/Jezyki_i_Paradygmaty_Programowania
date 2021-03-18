51619b36892d0ecea1e48f3fddfe72e1b43aa979
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2021-03-18 20:28:29 +0100

    Add Lab1

---
+++ Lab1/zadanie_1_6.hs
@@ -0,0 +1,29 @@
+f x = 2*x + 1
+
+{-
+*Main> f 5
+11
+-}
+
+{- Zadanie 1.6
+Uruchom interpreter jêzyka Haskell (polecenie ghci). 
+Wyznacz wartoœci nastepuj¹cych wyra¿eñ:
+
+*Main> 2+3*6
+20
+
+*Main> (12+6)*(7-2)
+90
+
+*Main> sqrt 3
+1.7320508075688772
+
+*Main> sqrt (12+4)
+4.0
+
+*Main> gcd 12 8
+4
+
+*Main> rem 12 5
+2
+-}
